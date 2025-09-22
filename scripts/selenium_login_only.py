949076c1424666332bd96a7f7467be3970c2a45c
Roger Cot Londres <roger.cot@haier-europe.com>
2025-09-22 12:43:52 +0200

    Qlik

--- scripts/selenium_login_only.py
+++ scripts/selenium_login_only.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
 
-# Opciones headless (sin interfaz gráfica)
+# Opciones headless
 opts = Options()
 opts.add_argument("--headless=new")
 opts.add_argument("--no-sandbox")
@@ -13,26 +16,33 @@ service = Service("/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service, options=opts)
 
 try:
-    # Página de demo (Herokuapp login)
-    url = "https://the-internet.herokuapp.com/login"
+    # URL de login de Qlik
+    url = "https://qliksense.candy.it/hub/stream/59bf718f-396a-4625-b61c-5233bf946956"
     driver.get(url)
 
     print("Página cargada:", driver.title)
 
-    # Localizar campos de login
-    username = driver.find_element(By.ID, "username")
-    password = driver.find_element(By.ID, "password")
-    login_btn = driver.find_element(By.CSS_SELECTOR, "button.radius")
+    wait = WebDriverWait(driver, 10)
 
-    # Rellenar (credenciales dummy)
-    username.send_keys("tomsmith")
-    password.send_keys("SuperSecretPassword!")
+    # Localizar los campos
+    username = wait.until(EC.presence_of_element_located((By.ID, "username-input")))
+    password = driver.find_element(By.ID, "password-input")
+    login_btn = driver.find_element(By.ID, "loginbtn")
+
+    # Completar credenciales
+    username.send_keys(os.environ["QLIK_USER"])
+    password.send_keys(os.environ["QLIK_PASSWORD"])
     login_btn.click()
 
-    # Comprobar si el login funcionó
-    msg = driver.find_element(By.ID, "flash").text
-    print("Mensaje después de login:", msg)
+    # Comprobar si hay mensaje de error o login exitoso
+    try:
+        msg = wait.until(EC.presence_of_element_located((By.ID, "error-message"))).text
+        if msg.strip():
+            print("Error en login:", msg)
+        else:
+            print("Login posiblemente exitoso (no hay error)")
+    except:
+        print("No se encontró mensaje de error, login posiblemente correcto")
 
 finally:
     driver.quit()
-
